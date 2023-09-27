8f5364707429eda6c51df05b3c5aec26a936e4af
Nabil Ahmed <105531667+nabil-github0@users.noreply.github.com>
2023-09-27 18:33:08 +0600

    Update Messages.tsx

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -37,7 +37,7 @@ const Messages: FC<MessagesProps> = ({ initialMessages, sessionId, chatId, sessi
     const scrollDownRef = useRef<HTMLDivElement | null>(null)
 
     const formatTimestamp = (timestamp: number) => {
-        return format(timestamp,"hh:mm a")
+        return format(timestamp,"h:mm a M-d-yy")
     }
 
     return (
